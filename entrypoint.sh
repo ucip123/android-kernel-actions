56d427ec367f933d90065bbdf92f60dddeff5452
ucip123 <98519520+ucip123@users.noreply.github.com>
2023-10-25 14:40:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,8 +36,8 @@ msg "Updating container..."
 apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
-    curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2 xz-utils
+    curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget gcc \
+    device-tree-compiler ca-certificates python3 python2 xz-utils build-essential
 cd /
 git clone https://github.com/ucip123/liquid_kernel_realme_even
 git config --global --add safe.directory /github/workspace
